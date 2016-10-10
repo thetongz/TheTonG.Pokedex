3f671e9294c07d435c6fdcdc8b46cf72b8b94d07
thetongz <miiz_tongz@live.com>
2016-10-10 23:56:57 +0700

    version 0.00.2

---
+++ dist/hello.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _function = require("./function");
+
+var name = "TheTonG";
+console.log((0, _function.sayHi)(name));
---
+++ src/hello.js
@@ -0,0 +1,3 @@
+import {sayHi} from './function'
+const name = "TheTonG"
+console.log(sayHi(name))
