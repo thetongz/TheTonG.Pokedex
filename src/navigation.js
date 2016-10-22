65b7ee712719b59179503637550a68c7393fda5f
thetongz <miiz_tongz@live.com>
2016-10-22 20:18:06 +0700

    add search button

--- src/navigation.js
+++ src/navigation.js
@@ -6,10 +6,16 @@ export class Navbar extends React.Component {
 
     render(){
         return(
-            <ul className="topnav">
-                <li><a >TheTonG.Pokedex</a></li>
-                <input type="text" className="right" name="search" placeholder="Search.."/>
-            </ul>
+            <section>
+                <ul className="topnav">
+                    <li><a >TheTonG.Pokedex</a></li>
+                    <div className="nav-right">
+                        <input type="text" name="search" placeholder="Pokemon's name"/>
+                        <button className=" search-button">Search</button>
+                    </div>
+                </ul>
+                
+            </section>            
         )
     }
 }
